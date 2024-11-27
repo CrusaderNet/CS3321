a6d71ae6b70addb5cfe24f557bc9b7a5dc3fb308
CrusaderNet <110443549+CrusaderNet@users.noreply.github.com>
2024-11-26 18:07:19 -0600

    Commenting the changes made in the file

--- entity.py
+++ entity.py
@@ -1,20 +1,25 @@
+# Login Entity Class
+# This class is used to store the loginID and password of the user
+# It has the following attributes: loginID, password and the following methods: getLoginID, setLoginID, getPassword, setPassword
+# Contributors: Seth Tourish, Amin Shawa, Sanabel Elkheir, Jerami Davis
+# Date: 11/26/2024
 class Login:
 
-    loginID = ""
-    password = ""
+    loginID = ""    # loginID of the user
+    password = ""   # password of the user
 
-    def __init__(self, loginID: str, password: str):
-        self.loginID = loginID
-        self.password = password
+    def __init__(self, loginID: str, password: str):    # Constructor
+        self.loginID = loginID  # Initialize
+        self.password = password    # Initialize
 
-    def getLoginID(self) -> str:
-        return self.loginID
+    def getLoginID(self) -> str:    # Getter for loginID
+        return self.loginID   # Return loginID
 
-    def setLoginID(self, loginID: str):
-        self.loginID = loginID
+    def setLoginID(self, loginID: str):  # Setter for loginID
+        self.loginID = loginID  # Set loginID
 
-    def getPassword(self) -> str:
-        return self.password
+    def getPassword(self) -> str:   # Getter for password
+        return self.password    # Return password
 
-    def setPassword(self, password: str):
-        self.password = password
+    def setPassword(self, password: str):   # Setter for password
+        self.password = password    # Set password
